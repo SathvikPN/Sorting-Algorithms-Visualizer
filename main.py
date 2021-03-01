c62b3b8bfb5e1ab5be8017d5db89866565d92ddb
Sathvika PN <sathvikasagar@gmail.com>
2021-03-01 08:15:35 +0530

    Added dropdown menus for selecting algo and sorting speed

--- main.py
+++ main.py
@@ -67,7 +67,22 @@ def sort():
 # - 2 buttons. (generate random array,start sorting)
 # - A canvas to draw array.
 
+UI_frame = Frame(window, width=900, height=300, bg=BLUE)
+UI_frame.grid(row=0, column=0, padx=10, pady=5)
 
+# dropdown to select Sorting Algorithms
+l1 = Label(UI_frame, text='Algorithm: ', bg=WHITE)
+l1.grid(row=0, column=0, padx=10, pady=5, sticky=W)
+algo_menu = ttk.Combobox(UI_frame, textvariable = algo_name, values = algo_list)
+algo_menu.grid(row=0, column=1, padx=5, pady=5)
+algo_menu.current(0)
+
+# dropdown to select sorting speed 
+l2 = Label(UI_frame, text="Sorting Speed: ", bg=WHITE)
+l2.grid(row=1, column=0, padx=10, pady=5, sticky=W)
+speed_menu = ttk.Combobox(UI_frame, textvariable=speed_name, values=speed_list)
+speed_menu.grid(row=1, column=1, padx=5, pady=5)
+speed_menu.current(0)
 
 # Display
 window.mainloop() # Infinite loop running application as long as window is not closed.
